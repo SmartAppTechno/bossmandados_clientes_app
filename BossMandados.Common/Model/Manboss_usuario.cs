1a0426f17e687f8f572b62fa09284cf74d817fd4
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2018-03-03 18:20:34 -0600

    Cambios para referencia de Common
    Se agregó Newtonsoft Json a .Droid
    Clases de Usuario y Cliente en Common.Model

---
+++ BossMandados.Common/Model/Manboss_usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+namespace Common.DBItems
+{
+    public class Manboss_usuario {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "nombre")]
+        public string Nombre { get; set; }
+
+        [JsonProperty(PropertyName = "correo")]
+        public string Correo { get; set; }
+
+        [JsonProperty(PropertyName = "usuario")]
+        public string Usuario { get; set; }
+    }
+}
