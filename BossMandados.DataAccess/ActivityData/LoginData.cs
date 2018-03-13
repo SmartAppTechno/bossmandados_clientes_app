303b137bc822a4161784103766d981dd47310276
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2018-03-13 10:21:03 -0600

    Cambio de librerias a PLC

---
+++ BossMandados.DataAccess/ActivityData/LoginData.cs
@@ -0,0 +1,19 @@
+﻿using BossMandados.Common.Model;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossMandados.DataAccess.ActivityData {
+    public class LoginData {
+        private MobileServiceClient client;
+        private IMobileServiceTable<Manboss_usuario> userTable;
+        public LoginData() {
+            
+            //client = new MobileServiceClient(GlobalValues.AppURL);
+
+        }
+    }
+}
