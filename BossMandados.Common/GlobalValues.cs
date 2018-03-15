f9378c284483b3e11eb2ea0a4621833aee87ba7d
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2018-03-15 12:15:03 -0600

    Clase GlobalValues en Common

---
+++ BossMandados.Common/GlobalValues.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossMandados.Common {
+    public class GlobalValues {
+        public const string AppURL = @"https://bossmandadosapi.azurewebsites.net";
+    }
+}
--- BossMandados.DataAccess/ActivityData/LoginData.cs
+++ BossMandados.DataAccess/ActivityData/LoginData.cs
@@ -1,8 +1,10 @@
-﻿using BossMandados.Common.Model;
+﻿using BossMandados.Common;
+using BossMandados.Common.Model;
 using Microsoft.WindowsAzure.MobileServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,9 +13,22 @@ namespace BossMandados.DataAccess.ActivityData {
         private MobileServiceClient client;
         private IMobileServiceTable<Manboss_usuario> userTable;
         public LoginData() {
-            
-            //client = new MobileServiceClient(GlobalValues.AppURL);
+            client = new MobileServiceClient(GlobalValues.AppURL);
+        }
 
+        public async Task<Manboss_cliente> Login(string mail) {
+            Manboss_cliente userReturn = null;
+            try {
+                Dictionary<string, string> param = new Dictionary<string, string>
+                {
+                    { "correo", mail },
+                };
+                var current = await client.InvokeApiAsync<Manboss_cliente>("Cliente", HttpMethod.Post, param);
+                userReturn = current;
+            }
+            catch {
+            }
+            return userReturn;
         }
     }
 }
