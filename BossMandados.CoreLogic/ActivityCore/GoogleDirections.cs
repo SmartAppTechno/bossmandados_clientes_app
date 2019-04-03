734ba17372ebda2094619ea1f665120d1e260a9a
Isaac Méndez <isaac95mendez@gmail.com>
2019-04-02 22:48:22 -0600

    GoogleDirectionsCore

---
+++ BossMandados.CoreLogic/ActivityCore/GoogleDirections.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BossMandados.Common.Model;
+using BossMandados.DataAccess.ActivityData;
+
+namespace BossMandados.CoreLogic.ActivityCore
+{
+    public class GoogleDirections
+    {
+        private GoogleDirectionsData data;
+        public string Direcciones { get; set; }
+
+        public GoogleDirections()
+        {
+            data = new GoogleDirectionsData();
+        }
+
+        public async Task<string> GetDirecciones(List<Manboss_mandados_ruta> lugares)
+        {
+            Direcciones = await data.ObtenerDirecciones(lugares);
+            return Direcciones;
+        }
+    }
+}
